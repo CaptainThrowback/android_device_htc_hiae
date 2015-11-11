11fbc650514ee5afa9afbae6cf77e65b056fc725
Captain Throwback <captainthrowback@hotmail.com>
2015-11-11 17:39:57 -0500

    Update prebuilt kernel & dtb
    Custom built kernel with f2fs, ntfs & exfat-nofuse
    
    Disable USB Storage (for now) until I figure out why connections are borked

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,8 +79,11 @@ TW_THEME := portrait_hdpi
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_EXFAT_FUSE := true
-TW_NO_SCREEN_BLANK := true
+#TW_NO_SCREEN_BLANK := true
+TW_NO_USB_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 #TWRP_EVENT_LOGGING := true
 
--- init/init_hiae.c
+++ init/init_hiae.c
@@ -34,17 +34,41 @@
 #include "log.h"
 #include "util.h"
 
-void cdma_properties(char cdma_subscription[],
-                     char default_network[],
-                     char operator_numeric[],
-                     char operator_alpha[]);
+void common_properties()
+{
+    property_set("rild.libargs", "-d /dev/smd0");
+    property_set("ro.ril.hsdpa.category", "14");
+    property_set("ro.ril.hsxpa", "4");
+    property_set("ro.ril.disable.cpc", "1");
+}
 
-void vendor_load_properties()
+void cdma_properties(char const default_cdma_sub[], char const default_network[])
+{
+    property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
+    property_set("ro.telephony.default_network", default_network);
+
+    property_set("telephony.lteOnCdmaDevice", "1");
+    property_set("ro.ril.svdo", "true");
+    property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420");
+    property_set("ro.ril.enable.sdr", "0");
+    property_set("ro.ril.enable.gea3", "1");
+    property_set("ro.ril.enable.a53", "1");
+    property_set("ro.ril.enable.r8fd", "1");
+    property_set("persist.radio.snapshot_enabled", "1");
+    property_set("persist.radio.snapshot_timer", "22");
+}
+
+void gsm_properties(char const default_network[])
+{
+    property_set("ro.telephony.default_network", default_network);
+    property_set("telephony.lteOnGsmDevice", "1");
+}
+
+void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
     char platform[PROP_VALUE_MAX];
     char bootmid[PROP_VALUE_MAX];
     char device[PROP_VALUE_MAX];
-    char devicename[PROP_VALUE_MAX];
     int rc;
 
     rc = property_get("ro.board.platform", platform);
@@ -55,13 +79,22 @@ void vendor_load_properties()
 
     if (strstr(bootmid, "2PQ930000")) {
         /* hiae_whl (Sprint) */
-        cdma_properties("1", "8", "310120", "Sprint");
+        cdma_properties("1", "8");
         property_set("ro.build.product", "htc_hiaewhl");
         property_set("ro.product.model", "2PQ93");
         property_set("ro.product.device", "htc_hiaewhl");
+    } else if (strstr(bootmid, "2PQ910000")) {
+        /* hiae_uhl (Europe) */
+        property_set("ro.build.product", "htc_hiaeuhl");
+        property_set("ro.build.fingerprint", "HTC/htc_hiaeuhl/htc_hiaeuhl:6.0/MRA58K/buildteam10132347:user/release-keys");
+        property_set("ro.product.device", "htc_hiaeuhl");
+        property_set("ro.product.model", "HTC One A9");
+        property_set("ro.telephony.default_network", "9");
+        property_set("telephony.lteOnGsmDevice", "1");
     } else {
         /* hiae_ul (GSM) */
         property_set("ro.build.product", "htc_hiaeul");
+        property_set("ro.build.fingerprint", "htc/hiaeul_00502/htc_hiaeul:6.0/MRA58K/635081.3:user/release-keys");
         property_set("ro.product.device", "htc_hiaeul");
         property_set("ro.product.model", "HTC One A9");
         property_set("ro.telephony.default_network", "9");
@@ -70,28 +103,4 @@ void vendor_load_properties()
 
     property_get("ro.product.device", device);
     ERROR("Found bootmid %s setting build properties for %s device\n", bootmid, device);
-}
-
-void cdma_properties(char default_cdma_sub[], char default_network[],
-                     char operator_numeric[], char operator_alpha[])
-{
-    property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
-    property_set("ro.telephony.default_network", default_network);
-    property_set("ro.cdma.home.operator.numeric", operator_numeric);
-    property_set("gsm.sim.operator.numeric", operator_numeric);
-    property_set("gsm.operator.numeric", operator_numeric);
-    property_set("ro.cdma.home.operator.alpha", operator_alpha);
-    property_set("gsm.sim.operator.alpha", operator_alpha);
-    property_set("gsm.operator.alpha", operator_numeric);
-
-    property_set("telephony.lteOnCdmaDevice", "1");
-    property_set("ro.cdma.subscribe_on_ruim_ready", "true");
-    property_set("ro.ril.svdo", "true");
-    property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420");
-    property_set("ro.ril.enable.sdr", "0");
-    property_set("ro.ril.enable.gea3", "1");
-    property_set("ro.ril.enable.a53", "1");
-    property_set("ro.ril.enable.r8fd", "1");
-    property_set("persist.radio.snapshot_enabled", "1");
-    property_set("persist.radio.snapshot_timer", "22");
 }
