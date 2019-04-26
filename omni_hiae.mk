01215dffea28bc8adb1a5c25b0868cefc813a57b
Captain Throwback <captainthrowback@hotmail.com>
2019-04-25 22:48:46 -0400

    hiae: cleanup and add new prebuilt kernel
    - One prebuilt kernel needed to decrypt AOSP
    - One prebuilt kernel needed to decrypt stock
    
    Change-Id: Ic7190dcbb74dc15c6cfd92e77b8544a034756213

--- omni_hiae.mk
+++ omni_hiae.mk
@@ -26,10 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Time Zone data for Recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hiae
 PRODUCT_NAME := omni_hiae
